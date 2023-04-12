e304cf2c94a85828c7f1db294a217fb9ff033308
Bogdan Radchenko <inroph@mail.ru>
2023-04-12 23:36:56 +0300

    Setup project

---
+++ BeerProject/Models/Beer.swift
@@ -0,0 +1,12 @@
+//
+//  Beer.swift
+//  BeerProject
+//
+//  Created by Богдан Радченко on 12.04.2023.
+//
+
+import Foundation
+
+struct Beer {
+    
+}
---
+++ BeerProject/Services/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  BeerProject
+//
+//  Created by Богдан Радченко on 12.04.2023.
+//
+
+import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+}
